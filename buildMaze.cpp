d4cbcfdb5a15451a731a9289f57e487dd224a791
David Rozmajzl <dmr121@zips.uakron.edu>
2018-11-19 20:01:40 -0500

    Allow user to input rows and columns and validate it

--- buildMaze.cpp
+++ buildMaze.cpp
@@ -4,12 +4,36 @@
 #include "DisjSets.h"
 #include "mazeCell.h"
 #include <iostream>
+#include <limits>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 int main(){
-  maze cool(3, 4);
-  cool.maze::printMaze();
+  int columns = 0, rows = 0;
+  cout << "Please enter the number of rows greater than 1: ";
+  cin >> rows;
+  while (cin.fail() || rows <= 1)
+  {
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    cout << "You must enter an integer greater than 1" << endl;
+    cout << "Please try again: ";
+    cin >> rows;
+  }
+
+  cout << "Please enter the number of columns greater than 1: ";
+  cin >> columns;
+  while (cin.fail() || columns <= 1)
+  {
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    cout << "You must enter an integer greater than 1" << endl;
+    cout << "Please try again: ";
+    cin >> columns;
+  }
+  maze myMaze(rows, columns);
+  myMaze.maze::printMaze();
   cout << endl;
   return 0;
 }
